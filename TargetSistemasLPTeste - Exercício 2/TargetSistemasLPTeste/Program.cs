e3bef8e71f7c844a7039c7545963375a6444be08
João Vítor Carvalho <mistingerr@gmail.com>
2022-02-08 11:44:11 -0300

    resolução das questões 2 e 5 teste Gupy TargetSistemas

---
+++ TargetSistemasLPTeste - Exercício 2/TargetSistemasLPTeste/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TargetSistemasLPTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("<[Calculadora de Fibonacci]> Digite um número: ");
+            int input = int.Parse(Console.ReadLine());
+            int zero = 0, fib = 1;
+
+            while (fib < input) 
+            {
+                zero += fib;
+                fib += zero;
+            }
+
+            if (fib == input || input <= 1 && input >= 0) 
+                Console.WriteLine("<[Calculadora de Fibonacci]> Numero pertence a sequência Fibonacci! :)");
+             else 
+                Console.WriteLine("<[Calculadora de Fibonacci]> Numero não pertence a sequência Fibonacci! :("); }
+        }
+    }
+}
---
+++ TargetSistemasLPTeste - Exercício 5/TargetSistemasLPTeste - Exercício 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TargetSistemasLPTeste___Exercício_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arrayFrase;
+            Console.Write("[Inversor de String] Digite uma frase : ");
+            string frase = Console.ReadLine();
+            arrayFrase = frase.ToCharArray();
+
+            char[] chars = frase.ToCharArray();
+            int numChars = chars.Length - 1;
+            for (int i = 0; i < numChars / 2; i++)
+            {
+                char temp = chars[i];
+                chars[i] = chars[numChars - i];
+                chars[numChars - i] = temp;
+            }
+            Console.WriteLine(chars);
+        }
+    }
+}
